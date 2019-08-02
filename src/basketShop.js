3f431ad70c9c5d13c436ce5c201e822fe3c075fc
Yura <ybeliaev@gmail.com>
2019-08-02 21:40:11 +0300

    some css

--- src/basketShop.js
+++ src/basketShop.js
@@ -4,7 +4,8 @@ export default class BasketShop extends React.Component {
   render() {
     return (
       <div className="basket-shop">
-        <i className="fa fa-trash-o" />
+        <i className="fa fa-shopping-basket" aria-hidden="true" />
+        <span>0</span>
       </div>
     );
   }
