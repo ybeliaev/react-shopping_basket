0674b5261cfb79f43bfea917e0ea810e104583ab
Yura <ybeliaev@gmail.com>
2019-08-02 21:43:23 +0300

    upp title

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox!!!</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Работа с корзиной товара</h2>
       <BasketShop />
     </div>
   );
