e4523e1d5710173ff731b4e691d55d209c588fb2
Yura <ybeliaev@gmail.com>
2019-08-03 15:05:16 +0300

    upp css

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox!!!</h1>
       <h2>Работа с корзиной товара</h2>
       <BasketShop />
     </div>
