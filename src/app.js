7a6f4266f125207dd6d3ceffa38e3f0bd7943e9d
Yura <ybeliaev@gmail.com>
2019-08-03 15:40:53 +0300

    add ItemList

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import React from "react";
 import BasketShop from "./basketShop";
+import ItemList from "./itemList";
 
 export default class App extends React.Component {
+  state = {
+    inputData: [
+      { id: 1, brand: "Apple", cost: 800, balance: 10 },
+      { id: 2, brand: "Xiaomi", cost: 700, balance: 8 },
+      { id: 3, brand: "Sumsung", cost: 750, balance: 9 },
+      { id: 4, brand: "Meizu", cost: 680, balance: 12 }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h2>Работа с корзиной товара</h2>
         <BasketShop />
+        <ItemList />
       </div>
     );
   }
---
+++ src/itemList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ItemList = () => {
+  return <div>1</div>;
+};
+
+export default ItemList;
